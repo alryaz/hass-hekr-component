7c6c9a93b119e813bb6f7c14e96b47ea12c51117
Telectroboy <telectroboy@hotmail.com>
2025-06-13 14:05:56 +0200

    Update hekr_data.py
    Tu dois faire :
    python
    Copier
    Modifier
    from homeassistant.core import HomeAssistant
    from homeassistant.helpers.typing import ConfigType
    Et remplacer dans la classe :
    
    python
    Copier
    Modifier
    def __init__(self, hass: HomeAssistantType)
    par :
    
    python
    Copier
    Modifier
    def __init__(self, hass: HomeAssistant)

--- custom_components/hekr/hekr_data.py
+++ custom_components/hekr/hekr_data.py
@@ -36,7 +36,8 @@ from homeassistant.const import (
     CONF_TIMEOUT,
 )
 from homeassistant.helpers.event import async_track_time_interval, async_track_point_in_time
-from homeassistant.helpers.typing import HomeAssistantType, ConfigType
+from homeassistant.core import HomeAssistant
+from homeassistant.helpers.typing import ConfigType
 from homeassistant.util.dt import now
 
 from custom_components.hekr.supported_protocols import SUPPORTED_PROTOCOLS
@@ -81,7 +82,7 @@ class FrameNumber(object):
 
 
 class HekrData:
-    def __init__(self, hass: HomeAssistantType):
+    def __init__(self, hass: HomeAssistant):
         if isinstance(hass.data.get(DOMAIN), HekrData):
             raise Exception("One instance of HekrData is already installed")
 
@@ -247,10 +248,10 @@ class HekrData:
 
             tasks.append(
                 self.hass.async_create_task(
-                    self.hass.config_entries.async_forward_entry_setup(config_entry, entity_domain)
+                    self.hass.config_entries.async_forward_entry_setups(config_entry, [entity_domain])
                 )
             )
-
+            
         return tasks
 
     def collect_devices_for_entry(
